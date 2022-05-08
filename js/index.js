5e0c24a3bdec573fbb55452838a15b3afb72ae65
LiamBergstrom <92992478+LiamBergstrom@users.noreply.github.com>
2022-05-08 17:07:38 +0200

    08/05-2022

--- js/index.js
+++ js/index.js
@@ -9,35 +9,37 @@ const toggleNavbarVisibility = () => {
 navbarToggle.addEventListener("click", toggleNavbarVisibility);
 
 var responsiveSlider = function () {
-  var slider = document.getElementById("slider");
-  var sliderWidth = slider.offsetWidth;
   var slideList = document.getElementById("slideWrap");
   var count = 1;
-  var items = 2;
-  // var items = slideList.querySelectorAll("li").length;
+
+  var slide = document.getElementById("oneSlide");
+  var slideWidth = slide.offsetWidth;
+
+  var items = slideList.querySelectorAll("li").length - 3;
+  // let working_img_width = window.innerWidth < 700 ? 100 : slideWidth;
 
   var prev = document.getElementById("prev");
   var next = document.getElementById("next");
 
   window.addEventListener("resize", function () {
-    sliderWidth = slider.offsetWidth;
+    slideWidth = oneSlide.offsetWidth;
   });
 
   var prevSlide = function () {
     if (count > 1) {
       count = count - 2;
-      slideList.style.left = "-" + count * sliderWidth + "px";
+      slideList.style.left = "-" + count * slideWidth + "px";
       count++;
     } else if ((count = 1)) {
       count = items - 1;
-      slideList.style.left = "-" + count * sliderWidth + "px";
+      slideList.style.left = "-" + count * slideWidth + "px";
       count++;
     }
   };
 
   var nextSlide = function () {
     if (count < items) {
-      slideList.style.left = "-" + count * sliderWidth + "px";
+      slideList.style.left = "-" + count * slideWidth + "px";
       count++;
     } else if ((count = items)) {
       slideList.style.left = "0px";
