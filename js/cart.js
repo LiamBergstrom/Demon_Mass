dd89c60c7b6914f1a45b6b3a655eb16f14d39e0d
LiamBergstrom <Liam.bergstrom@skola.taby.se>
2022-05-10 22:50:44 +0100

    10/05-2022

---
+++ js/cart.js
@@ -0,0 +1,26 @@
+let buttons = document.getElementsByTagName("button");
+
+const cart = [];
+
+for (var i = 0; i < buttons.length; i++) {
+  let button = buttons[i];
+  console.log(button);
+  button.addEventListener("click", function (event) {
+    console.clear();
+    console.log(event.target);
+    console.log(event.target.dataset.productSku);
+    cart.push(event.target.dataset.productSku);
+    console.log(cart);
+  });
+}
+
+const cartButtons = document.querySelectorAll(".cart-button");
+
+cartButtons.forEach((button) => {
+  button.addEventListener("click", cartClick);
+});
+
+function cartClick() {
+  let button = this;
+  button.classList.add("clicked");
+}
